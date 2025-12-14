a7cb806b52ef2375bd5e0c2b66651443810fe4b3
melaniewalsh <melwalsh@uw.edu>
2025-12-13 18:07:05 -0800

    fix var name

--- electron/notarize.js
+++ electron/notarize.js
@@ -9,10 +9,10 @@ exports.default = async function notarizing(context) {
   const appName = context.packager.appInfo.productFilename;
 
   const appleId = process.env.APPLE_ID;
-  const appleIdPassword = process.env.APPLE_ID_PASSWORD;
+  const appleIdPassword = process.env.APPLE_APP_SPECIFIC_PASSWORD;
 
   if (!appleId || !appleIdPassword) {
-    console.log('Skipping notarization: APPLE_ID or APPLE_ID_PASSWORD not set');
+    console.log('Skipping notarization: APPLE_ID or APPLE_APP_SPECIFIC_PASSWORD not set');
     return;
   }
 
